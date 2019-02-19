9bc9e61c05561b24c78ed10d680c935d5690e300
pin.park <pin0156@naver.com>
2019-02-19 23:54:55 +0900

    pin.park: add eval.py

--- CNN/CNN_pin/text_cnn.py
+++ CNN/CNN_pin/text_cnn.py
@@ -13,7 +13,7 @@ class TextCNN(object):
 
         # Placeholders for input, output and dropout
         self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name="input_x")
-        self.input_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")
+        self.input_y = tf.placeholder(tf.int32, [None, num_classes], name="input_y")
         self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
 
         # Embedding layer
