24aff17f39c13bd40e61a97cb434a7757199783e
hexists <hexists@daum.net>
2019-02-22 09:59:16 +0900

    bugfix code, data

--- CNN/CNN_hexists/data_helpers.py
+++ CNN/CNN_hexists/data_helpers.py
@@ -3,17 +3,17 @@ import re
 
 
 def clean_str(string):
-    '''
+    """
     Tokenization/string cleaning for all datasets except for SST.
     Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
-    '''
-    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
-    string = re.sub(r"\'s", " \'s", string)
-    string = re.sub(r"\'ve", " \'ve", string)
-    string = re.sub(r"n\'t", " n\'t", string)
-    string = re.sub(r"\'re", " \'re", string)
-    string = re.sub(r"\'d", " \'d", string)
-    string = re.sub(r"\'ll", " \'ll", string)
+    """
+    string = re.sub(r"[^A-Za-z0-9(),!?\"\`]", " ", string)
+    string = re.sub(r"\"s", " \"s", string)
+    string = re.sub(r"\"ve", " \"ve", string)
+    string = re.sub(r"n\"t", " n\"t", string)
+    string = re.sub(r"\"re", " \"re", string)
+    string = re.sub(r"\"d", " \"d", string)
+    string = re.sub(r"\"ll", " \"ll", string)
     string = re.sub(r",", " , ", string)
     string = re.sub(r"!", " ! ", string)
     string = re.sub(r"\(", " \( ", string)
@@ -22,15 +22,16 @@ def clean_str(string):
     string = re.sub(r"\s{2,}", " ", string)
     return string.strip().lower()
 
+
 def load_data_and_labels(positive_data_file, negative_data_file):
-    '''
+    """
     Loads MR polarity data from files, splits the data into words and generates labels.
     Returns split sentences and labels.
-    '''
+    """
     # Load data from files
-    positive_examples = list(open(positive_data_file, "r", encoding='utf-8').readlines())
+    positive_examples = list(open(positive_data_file, "r", encoding="utf-8").readlines())
     positive_examples = [s.strip() for s in positive_examples]
-    negative_examples = list(open(negative_data_file, "r", encoding='utf-8').readlines())
+    negative_examples = list(open(negative_data_file, "r", encoding="utf-8").readlines())
     negative_examples = [s.strip() for s in negative_examples]
     # Split by words
     x_text = positive_examples + negative_examples
@@ -41,10 +42,11 @@ def load_data_and_labels(positive_data_file, negative_data_file):
     y = np.concatenate([positive_labels, negative_labels], 0)
     return [x_text, y]
 
+
 def batch_iter(data, batch_size, num_epochs, shuffle=True):
-    '''
+    """
     Generates a batch iterator for a dataset.
-    '''
+    """
     data = np.array(data)
     data_size = len(data)
     num_batches_per_epoch = int((len(data)-1)/batch_size) + 1
